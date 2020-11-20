bbaf512fab2920b17a9b37dfce0af0e8f4d4eb45
Daigo Ichikawa <daidai.1231@icloud.com>
2020-11-20 13:15:14 +0900

    finised chat function

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,69 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="app">
+        <!-- 認証済みの時はメインの画面を表示 -->
+        <div v-if="signedIn">
+            <div id="nav">
+                <div class="amplify-sign-out">
+                    <!-- Amplify UIのSign Out用コンポーネントを追加 -->
+                    <amplify-sign-out />
+                    <!-- ログイン済みユーザのusernameを表示 -->
+                    ようこそ、{{username}} さん
+                </div>
+                <router-link to="/ai">AIアプリ</router-link> |
+                <router-link to="/chat">チャットアプリ</router-link>
+            </div>
+            <router-view />
+        </div>
+        <!-- 認証していない時はログイン画面を表示 -->
+        <div v-else>
+            <amplify-authenticator />
+        </div>
     </div>
-    <router-view/>
-  </div>
 </template>
 
-<style lang="less">
+
+<script>
+import { Auth } from 'aws-amplify'
+import { AmplifyEventBus } from 'aws-amplify-vue'
+
+window.LOG_LEVEL = 'VERBOSE';
+
+export default {
+  name: 'app',
+  data(){
+    return {
+      signedIn: false,
+      username: ''
+    }
+  },
+  async beforeCreate() {
+    // Auth.currentAuthenticatedUser()でユーザ情報を取得する。
+    // 取得できなければ認証ステータスをfalseに設定する
+    try {
+      let cognitoUser = await Auth.currentAuthenticatedUser()
+      this.signedIn = true
+      this.username = cognitoUser.username
+    } catch (err) {
+      this.signedIn = false
+    }
+    // 認証ステータスが変わった時に呼び出されるイベントを登録
+
+    AmplifyEventBus.$on('authState', async  info => {
+      if (info === 'signedIn') {
+        let cognitoUser = await Auth.currentAuthenticatedUser()
+        this.signedIn = true
+        this.username = cognitoUser.username
+      } else {
+        this.signedIn = false
+      }
+    });
+  }
+  };
+</script>
+
+<style src="./assets/style.css" />
+
+<!--<style lang="less">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -29,4 +84,4 @@
     }
   }
 }
-</style>
+</style>-->
--- src/main.js
+++ src/main.js
@@ -1,12 +1,22 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import locale from 'element-ui/lib/locale/lang/ja';
 
-Vue.config.productionTip = false
+import awsconfig from './aws-exports';
+import Amplify, * as AmplifyModules from 'aws-amplify';
+import { AmplifyPlugin } from 'aws-amplify-vue';
+
+Amplify.configure(awsconfig);
+Vue.use(AmplifyPlugin, AmplifyModules);
+
+
+Vue.config.productionTip = false;
+Vue.use(ElementUI, { locale });
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+    router,
+    render: h => h(App)
+}).$mount('#app');
